9d784dcb659b88a65948c47f05d3e240dec22f3e
praful upadhyaya <prafulupadhyaya@prafuls-MacBook-Air.local>
2025-02-09 01:22:06 +0530

    first commit

---
+++ src/main/java/com/example/controller/ControllerLayer.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ *
+ * @author prafulupadhyaya
+ */
+
+@RestController
+public class ControllerLayer {
+    @GetMapping("/hello")
+    public String greet(){
+        return "Hi From CICD Pipeline";
+    }
+}
